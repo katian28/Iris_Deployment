2fbb9d8202f421abbe3ba6c4e87378caf3661b7d
Katia Nkurunziza <146153021+katian28@users.noreply.github.com>
2024-11-21 22:52:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import numpy as np
 import joblib
 
 def predict(data):
-    joblib==1.2.0
-    streamlit==1.27.2
-    scikit-learn==0.23.1
-    pandas==1.5.2
     clf = joblib.load("rf_model.sav", mmap_mode='r') 
     return clf.predict(data)
 
